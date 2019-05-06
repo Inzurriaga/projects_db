1a0e44a8e8624e57b1427a38b9026a410ac6e528
Devin Kapla <devinkapla@gmail.com>
2019-05-06 14:29:18 -0600

    Boiler plate api end points

--- server.js
+++ server.js
@@ -7,3 +7,43 @@ const database = require('knex')(configuration);
 
 app.use(express.json())
 app.listen(app.get('port'), () => console.log(`Listening on port ${app.get('port')}`));
+
+app.get('/api/projects', (req, res) => {
+
+})
+
+app.get('/api/palettes', (req, res) => {
+
+})
+
+app.get('/api/projects/:id', (req, res) => {
+
+})
+
+app.get('/api/palettes/:id', (req, res) => {
+
+})
+
+app.post('/api/projects', (req, res) => {
+
+})
+
+app.post('/api/palettes', (req, res) => {
+
+})
+
+app.patch('/api/projects/:id', (req, res) => {
+
+})
+
+app.patch('/api/palettes/:id', (req, res) => {
+
+})
+
+app.delete('/api/project/:id', (req, res) => {
+
+})
+
+app.delete('/api/palettes/:id', (req, res) => {
+
+})
