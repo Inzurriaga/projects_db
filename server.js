e3ec3102bed8285d23c1ab6492fa951049d9db28
Devin Kapla <devinkapla@gmail.com>
2019-05-06 14:09:13 -0600

    Setup server file imports

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
-
+const express = require('express');
+const app = express();
+app.set('port', process.env.PORT || 3001)
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
+
+app.use(express.json())
+app.listen(app.get('port'), () => console.log(`Listening on port ${app.get('port')}`));
