9df6e5dbf6f455aab8944552c2c0bfc4c5699eb0
Devin Kapla <devinkapla@gmail.com>
2019-05-07 16:42:01 -0600

    Add seed file and mock data

---
+++ db/seeds/projectsData.js
@@ -0,0 +1,48 @@
+const projectsData = [
+  {
+    name: 'Project 1',
+    desc: 'Obligitory',
+    palettes: [
+      {
+        name: 'Warm Colors',
+        color1: '111111',
+        color2: '222222',
+        color3: '333333',
+        color4: '444444',
+        color5: '555555',
+      },
+      {
+        name: 'cool Colors',
+        color1: '666666',
+        color2: '777777',
+        color3: '888888',
+        color4: '999999',
+        color5: '101010',
+      }
+    ]
+  },
+  {
+    name: 'Project 2',
+    desc: 'Obligitory #2',
+    palettes: [
+      {
+        name: 'other Colors',
+        color1: '283734',
+        color2: '834923',
+        color3: '983450',
+        color4: '239423',
+        color5: '232324',
+      },
+      {
+        name: 'more Colors',
+        color1: '643322',
+        color2: '777577',
+        color3: '888889',
+        color4: '994999',
+        color5: '101011',
+      }
+    ]
+  }
+]
+
+module.exports = projectsData;
