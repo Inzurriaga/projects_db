37758852841f1b7de094d9dff5a54716d2bf7e8e
Devin Kapla <devinkapla@gmail.com>
2019-05-08 15:18:15 -0600

    Setup skeleton tests and test-dev

--- app.spec.js
+++ app.spec.js
@@ -1,4 +1,4 @@
-const app = require('./server')
+const app = require('./app')
 const request = require('supertest')
 
 const environment = process.env.NODE_ENV || 'development';
@@ -10,74 +10,76 @@ describe('/api', () => {
     await database.seed.run()
   })
   describe('GET all', () => {
-    it('Should return all projects and status 200', () => {
+    it('Should return all projects and status 200', async () => {
+      const response = await request(app).get('/api/projects')
 
+      expect(response.status).toBe(200)
     })
-    it('Should return all palettes and status 200', () => {
+    it.skip('Should return all palettes and status 200', () => {
 
     })
   })
   describe('GET /projects/:id', () => {
-    it('Should a specific project and status 200', () => {
+    it.skip('Should a specific project and status 200', () => {
 
     })
-    it('Should return error msg and status 404 if none found', () => {
+    it.skip('Should return error msg and status 404 if none found', () => {
 
     })
   })
   describe('GET /projects/palettes/:id', () => {
-    it('Should a specific palette and status 200', () => {
+    it.skip('Should a specific palette and status 200', () => {
 
     })
-    it('Should return error msg and status 404 if none found', () => {
+    it.skip('Should return error msg and status 404 if none found', () => {
 
     })
   })
   describe('POST /projects', () => {
-    it('Should add project and status 201', () => {
+    it.skip('Should add project and status 201', () => {
 
     })
-    it('Should return error msg and status 422 if incorrect request', () => {
+    it.skip('Should return error msg and status 422 if incorrect request', () => {
 
     })
   })
   describe('POST /projects/palettes', () => {
-    it('Should add palettes and status 201', () => {
+    it.skip('Should add palettes and status 201', () => {
 
     })
-    it('Should return error msg and status 422 if incorrect request', () => {
+    it.skip('Should return error msg and status 422 if incorrect request', () => {
 
     })
   })
   describe('PATCH /projects/:id', () => {
-    it('Should update project and return status 200', () => {
+    it.skip('Should update project and return status 200', () => {
 
     })
-    it('Should return error msg and status 422 if incorrect request', () => {
+    it.skip('Should return error msg and status 422 if incorrect request', () => {
 
     })
   })
   describe('PATCH /projects/palettes/:id', () => {
-    it('Should update palettes and return status 200', () => {
+    it.skip('Should update palettes and return status 200', () => {
 
     })
-    it('Should return error msg and status 422 if incorrect request', () => {
+    it.skip('Should return error msg and status 422 if incorrect request', () => {
 
     })
   })
   describe('DELETE /projects/:id', () => {
-    it('Should remove project and return status 200', () => {
+    it.skip('Should remove project and return status 200', () => {
 
     })
-    it('Should return error msg and status 404 if project not found', () => {
+    it.skip('Should return error msg and status 404 if project not found', () => {
 
     })
   })
   describe('DELETE /projects/palettes/:id', () => {
-    it('Should remove palette and return status 200', () => {
+    it.skip('Should remove palette and return status 200', () => {
 
     })
-    it('Should return error msg and status 404 if palette not found', () => {
+    it.skip('Should return error msg and status 404 if palette not found', () => {
 
     })
   })
